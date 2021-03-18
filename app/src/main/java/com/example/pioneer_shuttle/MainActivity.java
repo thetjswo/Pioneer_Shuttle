97a26f75a72559257e815698df5941ef3c73c692
leeseonjae <tjswodi96@gmail.com>
2021-03-19 01:44:16 +0900

    first work

---
+++ app/src/main/java/com/example/pioneer_shuttle/MainActivity.java
@@ -0,0 +1,1 @@
+package com.example.pioneer_shuttle;import androidx.appcompat.app.AppCompatActivity;import android.os.Bundle;public class MainActivity extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.activity_main);    }}
---
+++ app/src/main/java/com/example/pioneer_shuttle/Main_Screen.java
@@ -0,0 +1,1 @@
+package com.example.pioneer_shuttle;import android.os.Bundle;import androidx.appcompat.app.AppCompatActivity;public class Main_Screen extends AppCompatActivity {    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);        setContentView(R.layout.main_screen);    }}
